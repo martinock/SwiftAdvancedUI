478a50e9a0c8107fbcbfa0db7005d3efb860da76
Martino Christanto Khuangga <13514084@std.stei.itb.ac.id>
2018-11-07 08:27:03 +0700

    Fix keyboard interaction in scroll view

--- Advanced UI 1/ScrollViewViewController.swift
+++ Advanced UI 1/ScrollViewViewController.swift
@@ -15,12 +15,16 @@ class ScrollViewViewController: UIViewController {
     @IBOutlet weak var favBeverageInput: UITextField!
     @IBOutlet weak var nicknameInput: UITextField!
     @IBOutlet weak var additionalInfoInput: UITextField!
+    @IBOutlet weak var scrollView: UIScrollView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         let tap = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
         self.view.addGestureRecognizer(tap)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardDidShow(_:)), name: UIResponder.keyboardDidShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardDidHide(_:)), name: UIResponder.keyboardDidHideNotification, object: nil)
     }
     
     @objc func hideKeyboard() {
@@ -35,11 +39,37 @@ class ScrollViewViewController: UIViewController {
         self.present(alert, animated: true, completion: nil)
     }
     
-//    func adjustInsetForKeyboardShow(_ show: Bool, notification: Notification) {
-//        let userInfo = notification.userInfo ?? [:]
-//        let keyboardFrame = (userInfo[UIKeyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
-//        let adjustmentHeight = (keyboardFrame.height + 20) * (show ? 1 : -1)
-//        scrollView.contentInset.bottom += adjustmentHeight
-//        scrollView.scrollIndicatorInsets.bottom += adjustmentHeight
-//    }
+    @objc func keyboardDidShow(_ notification: Notification) {
+        adjustInsetForKeyboardShow(true, notification: notification)
+    }
+    
+    @objc func keyboardDidHide(_ notification: Notification) {
+        adjustInsetForKeyboardShow(false, notification: notification)
+    }
+    
+    func adjustInsetForKeyboardShow(_ show: Bool, notification: Notification) {
+        let userInfo = notification.userInfo ?? [:]
+        let keyboardFrame = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        let adjustmentHeight = (keyboardFrame.height + 20) * (show ? 1 : -1)
+        scrollView.contentInset.bottom += adjustmentHeight
+        scrollView.scrollIndicatorInsets.bottom += adjustmentHeight
+    }
+}
+
+extension ScrollViewViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        switch textField {
+        case nameInput:
+            addressInput.becomeFirstResponder()
+        case additionalInfoInput:
+            hobbiesInput.becomeFirstResponder()
+        case hobbiesInput:
+            favFoodInput.becomeFirstResponder()
+        case additionalInfoInput:
+            hideKeyboard()
+        default:
+            return false
+        }
+        return true
+    }
 }
